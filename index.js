a6373e18e8f233197e37918ccc4d023278aa74a1
Mario Aguilar <marioaguilar740@gmail.com>
2023-04-19 17:14:15 +0200

    Se han instalado las dependencias para los middlewares, se genero el SECRET y se configuro el .env

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const cors = require('cors');
 const app = express ();
-const PORT = 3000 
-app.use (express.json());
-app.listen (PORT, () => {
-    console.log(`Servidor levantado en puerto ${PORT}` );
+
+require('dotenv').config();
+
+let corsOption = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}
+
+app.use(cors())
+
+app.use(express.json());
+
+// const rutas = require('./rutas')
+// app.use('/api', rutas)
+
+const db = require('./db')
+const PORT = process.env.PORT || 3000 
+
+db.then(() => {
+    app.listen (PORT, () => {
+        console.log(`Servidor levantado en puerto ${PORT}` );
+    })
 })
+    .catch((err) => console.log (err.message))
+
