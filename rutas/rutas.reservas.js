88cc36b2a3e63b1f43b4ccb02ace4d1eeea06e81
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-08 16:19:15 +0200

    Se han modificado las rutas para acceder correctamente desde el Frontend

--- rutas/rutas.reservas.js
+++ rutas/rutas.reservas.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
 const ReservasCtrl = require('../controladores/reservasControlador')
+const verifyToken = require('../middlewares/verifyToken')
+const isAdmin = require('../middlewares/privAdmin')
 
-router.get('/reservas', ReservasCtrl.apiGetAllReservas)
-router.get('/reservas/:id', ReservasCtrl.apiGetReservaById)
-router.post('/reservas', ReservasCtrl.apiAddReserva)
-router.put('/reservas/:id', ReservasCtrl.apiUpdateReserva)
-router.delete('/reservas/:id', ReservasCtrl.apiDeleteReserva)
+router.get('/reservas', verifyToken, isAdmin, ReservasCtrl.apiGetAllReservas)
+router.get('/reservas/usuarios', verifyToken, ReservasCtrl.apiGetReservaById)
+router.post('/nuevareserva', verifyToken, ReservasCtrl.apiAddReserva)
+router.put('/reservas/:id', verifyToken, ReservasCtrl.apiUpdateReserva)
+router.delete('/reservas/:id', verifyToken, ReservasCtrl.apiDeleteReserva)
+router.get('/reservas/usuario', verifyToken, ReservasCtrl.getReservaUsuario)
+
 
 module.exports = router
