4f4d5354decc740804f197a86caef7afd0c3d09e
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-08 16:21:35 +0200

    Se han modificado las rutas de las mesas

--- rutas/rutas.mesas.js
+++ rutas/rutas.mesas.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const MesasCtrl = require('../controladores/mesasControlador')
+const verifyToken = require('../middlewares/verifyToken')
+const isAdmin = require('../middlewares/privAdmin')
 
 router.get('/mesas', MesasCtrl.apiGetAllMesas)
 router.get('/mesas/:id', MesasCtrl.apiGetMesasById)
-router.post('/mesas', MesasCtrl.apiAddMesa)
-router.put('/mesas/:id', MesasCtrl.apiUpdateMesa)
-router.delete('/mesas/:id', MesasCtrl.apiDeleteMesa)
+router.post('/mesas',verifyToken, isAdmin, MesasCtrl.apiAddMesa)
+router.put('/mesas/:id', verifyToken, isAdmin, MesasCtrl.apiUpdateMesa)
+router.delete('/mesas/:id', verifyToken, isAdmin, MesasCtrl.apiDeleteMesa)
 
 module.exports = router
