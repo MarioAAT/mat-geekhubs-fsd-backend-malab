a4cb75f84c999dc8647ca716dc12815f8f32ae28
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-08 16:18:38 +0200

    Se han modificado y agregado Rutas para modificar datos de usuarios

--- rutas/rutas.usuario.js
+++ rutas/rutas.usuario.js
@@ -3,10 +3,11 @@ const UsuarioCtrl = require('../controladores/usuarioControlador')
 const verifyToken = require('../middlewares/verifyToken')
 const esAdmin = require('../middlewares/privAdmin')
 
-router.get('/usuarios', verifyToken, esAdmin, UsuarioCtrl.apiGetAllUsuarios)
-router.get('/usuarios/:id', verifyToken, UsuarioCtrl.apiGetUsuarioById)
-router.post('/usuarios', UsuarioCtrl.apiAddUsuario)
-router.put('/usuarios/:id', verifyToken, esAdmin, UsuarioCtrl.apiUpdateUsuario)
+router.get('/allusuarios', verifyToken, esAdmin, UsuarioCtrl.apiGetAllUsuarios)
+router.get('/usuario/perfil', verifyToken, UsuarioCtrl.apiGetUsuarioById)
+router.post('/nuevousuario', UsuarioCtrl.apiAddUsuario)
+router.put('/usuarios/edit', verifyToken, UsuarioCtrl.apiUpdateUsuario)
+router.put('/usuario/edit/admin/:id', verifyToken, UsuarioCtrl.editAdminUsuario)
 router.delete('/usuarios/:id',verifyToken, esAdmin, UsuarioCtrl.apiDeleteUsuario)
 
 module.exports = router 
